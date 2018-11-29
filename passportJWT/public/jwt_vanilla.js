607aa1456d31816ad25145252e067294d5f50c09
rsol7 <rsolm72@gmail.com>
2018-11-28 23:49:38 -0700

    es6 functionality

--- passportJWT/public/jwt_vanilla.js
+++ passportJWT/public/jwt_vanilla.js
@@ -1,49 +1,86 @@
 
 // ----------------------------------------------------------------------- XMLHttpRequest
 // make the request to the login endpoint
-function getToken() {
-    var loginUrl = "http://localhost:2018/login"
-    var xhr = new XMLHttpRequest();
+// function getToken() {
+//     var loginUrl = "http://localhost:2018/login"
+//     var xhr = new XMLHttpRequest();
+//     var userElement = document.getElementById('username');
+//     var passwordElement = document.getElementById('password');
+//     var tokenElement = document.getElementById('token-holder');
+//     var user = userElement.value;
+//     var password = passwordElement.value;
+
+//     xhr.open('POST', loginUrl, true);
+//     xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+//     xhr.addEventListener('load', function() {
+//         var responseObject = JSON.parse(this.response);
+//         console.log(responseObject);
+//         if (responseObject.token) {
+//             tokenElement.innerHTML = responseObject.token;
+//         } else {
+//             tokenElement.innerHTML = "No token received";
+//         }
+//     });
+
+//     var sendObject = JSON.stringify({name: user, password: password});
+//     console.log('going to send', sendObject);
+//     xhr.send(sendObject);
+// }
+
+//make the request to the secret API endpoint
+// function getSecret() {
+//     var url = "http://localhost:2018/secret"
+//     var xhr = new XMLHttpRequest();
+//     var tokenElement = document.getElementById('token-holder');
+//     var resultElement = document.getElementById('result');
+//     xhr.open('GET', url, true);
+//     xhr.setRequestHeader("Authorization", "bearer " + tokenElement.innerHTML);
+//     xhr.addEventListener('load', function() {
+//         var responseObject = JSON.parse(this.response);
+//         console.log(responseObject);
+//         resultElement.innerHTML = this.responseText;
+//     });
+
+//     xhr.send(null);
+// }
+
+// ---------------------------------------------------------------------------------- es6 fetch 
+function getToken(){
+    var url = "http://localhost:2018/login";
     var userElement = document.getElementById('username');
     var passwordElement = document.getElementById('password');
     var tokenElement = document.getElementById('token-holder');
     var user = userElement.value;
     var password = passwordElement.value;
+    var data = {name: user, password: password};
 
-    xhr.open('POST', loginUrl, true);
-    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-    xhr.addEventListener('load', function() {
-        var responseObject = JSON.parse(this.response);
-        console.log(responseObject);
-        if (responseObject.token) {
-            tokenElement.innerHTML = responseObject.token;
-        } else {
-            tokenElement.innerHTML = "No token received";
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers:{
+            'Content-Type': 'application/json; charset=UTF-8',
         }
-    });
-
-    var sendObject = JSON.stringify({name: user, password: password});
-    console.log('going to send', sendObject);
-    xhr.send(sendObject);
+    }).then(res => res.json())
+    .then(response =>  {
+        tokenElement.innerHTML = response.token;
+        console.log('Success:', JSON.stringify(response) );      
+    })
+    .catch(error => console.error('Error:', error));
 }
-
-//make the request to the secret API endpoint
-function getSecret() {
-    var url = "http://localhost:2018/secret"
-    var xhr = new XMLHttpRequest();
+    
+function getSecret(){
+    var url = "http://localhost:2018/secret";
     var tokenElement = document.getElementById('token-holder');
-    var resultElement = document.getElementById('result');
-    xhr.open('GET', url, true);
-    xhr.setRequestHeader("Authorization", "bearer " + tokenElement.innerHTML);
-    xhr.addEventListener('load', function() {
-        var responseObject = JSON.parse(this.response);
-        console.log(responseObject);
-        resultElement.innerHTML = this.responseText;
-    });
 
-    xhr.send(null);
+    fetch(url, {
+        method: 'GET',
+        headers:{
+            'Authorization': `bearer ${tokenElement.innerHTML}`
+        }
+    }).then(res => res.json())
+    .then(response => console.log('Success:', response))
+    .catch(error => console.error('Error:', error)); 
 }
-
 
 ///////// uploading file
 function previewFile() {
